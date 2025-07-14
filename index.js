3adbcb0ffebc8bb877371d6434d2edad77914300
Girlsaidwhat <girlsaidwhat@gmail.com>
2025-07-14 00:40:39 -0400

    Delete index.js
    Delete misplaced index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>The Scope of Morgellons</h1>
-      <p>This is the homepage. You’re almost there!</p>
-    </main>
-  )
-}
