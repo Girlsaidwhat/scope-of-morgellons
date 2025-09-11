f14575bdfa7d25d4228f360eb2e8524c686c9fc3
Eala Clarke <girlsaidwhat@gmail.com>
2025-09-11 19:57:45 -0400

    Add /home-test route with static aside image (sanity check page)

--- pages/home-test.js
+++ pages/home-test.js
@@ -1,15 +1,33 @@
-// pages/home-test.js
 export default function HomeTest() {
   return (
     <main style={{ maxWidth: 1000, margin: "0 auto", padding: 24 }}>
-      <header style={{ display: "flex", justifyContent: "space-between", alignItems: "flex-start", gap: 20 }}>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          gap: 20,
+        }}
+      >
         <h1 style={{ fontSize: 28, margin: 0 }}>Home Test</h1>
-        <div style={{ width: 315, height: 429, border: "1px solid #e5e7eb", borderRadius: 8, overflow: "hidden" }}>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
+        <div
+          style={{
+            width: 315,
+            height: 429,
+            border: "1px solid #e5e7eb",
+            borderRadius: 8,
+            overflow: "hidden",
+          }}
+        >
           <img
             src="/story_placeholder-idx36704.jpg"
             alt="Fill in your story"
-            style={{ width: "100%", height: "100%", objectFit: "contain", display: "block" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "contain",
+              display: "block",
+            }}
           />
         </div>
       </header>
